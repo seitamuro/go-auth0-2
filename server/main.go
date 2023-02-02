0712f60ea3755610c0bbfe26753b9d550d905bb0
seitamuro <seitamuro0210@gmail.com>
2023-02-02 21:43:51 +0900

    main serve

---
+++ server/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	v1 "github.com/seitamuro/go-auth0-2/server/handler/v1"
+)
+
+const (
+	port = 8000
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1", v1.HandleIndex)
+
+	addr := fmt.Sprintf(":%d", port)
+
+	log.Printf("Listening on %s", addr)
+	if err := http.ListenAndServe(addr, mux); err != nil {
+		log.Fatal(err)
+	}
+}
