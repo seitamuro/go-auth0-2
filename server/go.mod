9fc7d80e2a760e6a208bd682bc0a63a17ddffcb4
seitamuro <seitamuro0210@gmail.com>
2023-02-03 21:27:21 +0900

    add main

--- server/go.mod
+++ server/go.mod
@@ -3,6 +3,8 @@ module github.com/seitamuro/go-auth0-2/server
 go 1.19
 
 require (
-	github.com/auth0/go-jwt-middleware v1.0.1 // indirect
-	github.com/form3tech-oss/jwt-go v3.2.2+incompatible // indirect
+	github.com/auth0/go-jwt-middleware v1.0.1
+	github.com/form3tech-oss/jwt-go v3.2.2+incompatible
+	github.com/joho/godotenv v1.4.0
+	github.com/rs/cors v1.8.3
 )
--- server/main.go
+++ server/main.go
@@ -1,25 +1,59 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
+	"github.com/rs/cors"
 	v1 "github.com/seitamuro/go-auth0-2/server/handlers/v1"
-)
-
-const (
-	port = 8000
+	"github.com/seitamuro/go-auth0-2/server/handlers/v1/users/me"
+	"github.com/seitamuro/go-auth0-2/server/middlewares/auth0"
 )
 
 func main() {
+	// .envファイル読み込み
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	url := os.Getenv("API_URL")
+	domain := os.Getenv("AUTH0_DOMAIN")
+	clientID := os.Getenv("AUTH0_CLIENT_ID")
+
+	// 公開鍵を取得する
+	jwks, err := auth0.FetchJWKS(domain)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// domain, clientID, 公開鍵を元にJWTMiddlewareを作成する
+	jwtMiddleware, err := auth0.NewMiddleware(domain, clientID, jwks)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	mux := http.NewServeMux()
+	// /v1へのリクエストの場合のハンドラを登録
 	mux.HandleFunc("/v1", v1.HandleIndex)
+	// /v1/users/meへのリクエストの場合のハンドラを登録
+	// auth0.UseJWTでラップし、ハンドラを呼ぶ前にJWT認証を行う
+	mux.Handle("/v1/users/me", auth0.UseJWT(http.HandlerFunc(me.HandleIndex)))
 
-	addr := fmt.Sprintf(":%d", port)
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowedHeaders:   []string{"Authorization", "Content-Type"},
+		AllowCredentials: true,
+		Debug:            true,
+	})
 
-	log.Printf("Listening on %s", addr)
-	if err := http.ListenAndServe(addr, mux); err != nil {
+	// リクエスト前にJWTMiddlewareをContextに埋め込むためのMiddlewareを追加
+	wrappedMux := auth0.WithJWTMiddleware(jwtMiddleware)(mux)
+	wrappedMux = c.Handler(wrappedMux)
+
+	log.Printf("Listening on %s", url)
+	if err := http.ListenAndServe(url, wrappedMux); err != nil {
 		log.Fatal(err)
 	}
 }
