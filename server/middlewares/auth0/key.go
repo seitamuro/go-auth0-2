fe81bad0ac83daadbcb0a7bfb6631ba4db939e15
seitamuro <seitamuro0210@gmail.com>
2023-02-03 21:18:58 +0900

    add API for jwks.json

---
+++ server/middlewares/auth0/key.go
@@ -0,0 +1,33 @@
+package auth0
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type JSONWebKeys struct {
+	Kty string   `json:"kty"`
+	Kid string   `json:"kid"`
+	Use string   `json:"use"`
+	N   string   `json:"n"`
+	E   string   `json:"e"`
+	X5c []string `json:"x5c"`
+}
+
+type JWKS struct {
+	Keys []JSONWebKeys `json:"keys"`
+}
+
+func FetchJWKS(auth0Domain string) (*JWKS, error) {
+	resp, err := http.Get(fmt.Sprintf("https://%s/.well-known/jwks.json", auth0Domain))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	jwks := &JWKS{}
+	err = json.NewDecoder(resp.Body).Decode(jwks)
+
+	return jwks, err
+}
